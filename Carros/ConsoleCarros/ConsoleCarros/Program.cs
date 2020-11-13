78a36014756f5b8565c8300a6c824e52f4905ac3
Ari Proenca <ariproenca@outlook.com>
2020-11-13 15:24:13 -0300

    Primeiro commit

---
+++ Carros/ConsoleCarros/ConsoleCarros/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de carros vendidos:");
+            int numeroCarrosVendidos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor total das suas vendas:");
+            double valorTotalVendas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do salário fixo:");
+            double valorSalarioFixo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a comissão fixa por cada carro vendido:");
+            double valorComissaoCarroVendido = Convert.ToDouble(Console.ReadLine());
+
+            double salarioFinalVendedor = CalcularSalarioFinalVendedor(valorSalarioFixo, valorComissaoCarroVendido, numeroCarrosVendidos, valorTotalVendas);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(salarioFinalVendedor);
+            Console.ReadKey();
+            
+        }
+
+        public static double CalcularSalarioFinalVendedor( double salarioFixoVendedor,
+                                                    double valorComissaoCarroVendido,
+                                                    int numeroCarrosVendidos,
+                                                    double valorTotalVendas)
+        {
+            double totalValorComissaoCarroVendido = Convert.ToDouble(valorComissaoCarroVendido * numeroCarrosVendidos);
+
+            return Convert.ToDouble(salarioFixoVendedor + totalValorComissaoCarroVendido + (Convert.ToDouble(valorTotalVendas * 1.05)));
+        }
+
+    }
+}
---
+++ Telecomunicacoes/ConsoleTelecomunicacoes/ConsoleTelecomunicacoes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleTelecomunicacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a duração da chamada em segundos:");
+            int duracaoChamada = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor por minuto(utilize vírgula):");
+            double valorMinuto = Convert.ToDouble(Console.ReadLine());
+
+            double valorChamada = CalcularChamada(duracaoChamada, valorMinuto);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(valorChamada);
+            
+            Console.ReadKey();
+
+        }
+
+        public static double CalcularChamada(int duracaoChamada, double valorMinuto)
+        {
+
+            int _duracaoChamada = 0;
+            double _adicionalChamada = 0;
+
+            if (duracaoChamada <= 60)
+            {
+                return Convert.ToDouble(valorMinuto);
+            }
+            else
+            {
+                _duracaoChamada = duracaoChamada - 60;
+
+                if (_duracaoChamada >= 6)
+                {
+                    _adicionalChamada = _duracaoChamada / 6;
+                }
+            }
+
+            return valorMinuto + Convert.ToDouble(_adicionalChamada * 0.01);
+
+        }
+
+    }
+}
